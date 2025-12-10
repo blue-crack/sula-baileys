1802e7f0fc7bf3362b161b94b4788e12966873bd
Thenux <scieovlogs12345@gmail.com>
2025-12-10 08:27:44 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-const { makeWASocket } = require('@thenux/sula-baileys');
-console.log('Package loaded successfully!');
+       const { makeWASocket } = require('@thenux/sula-baileys');
+       console.log('Package loaded successfully!');
+
+        const sock = makeWASocket({ /* config */ });
+        sock.requestPairingCode('94757096717').then(code => console.log('Pairing Code:', code));
+       
